3496681e154f2682955987a3793ca9107714e3ae
Wanderlei Santos <wanderleisantosdias@gmail.com>
2018-03-01 23:05:35 -0300

    Layout Swipe

--- App.js
+++ App.js
@@ -1,58 +1,228 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  Image,
+  Animated, 
+  PanResponder
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
-type Props = {};
-export default class App extends Component<Props> {
+const ARTICLES = [
+  {id: "1", uri: require('./src/imgs/1.png')},
+  {id: "2", uri: require('./src/imgs/2.jpg')},
+  {id: "3", uri: require('./src/imgs/3.jpg')},
+  {id: "4", uri: require('./src/imgs/4.jpg')}
+];
+
+export default class App extends Component{
+  
+  constructor(props) {
+    super(props)
+
+    this.position = new Animated.ValueXY()
+    this.state = {
+      currentIndex: 0
+    }
+
+  }
+
+  componentWillMount() {
+
+    this.PanResponder = PanResponder.create({
+
+      onStartShouldSetPanResponder: (e, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+
+        this.position.setValue({ y: gestureState.dy })
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+
+        if (-gestureState.dy > 50 && -gestureState.vy > 0.7) {
+
+          Animated.timing(this.position, {
+            toValue: ({ x: 0, y: -SCREEN_HEIGHT }),
+            duration: 400
+          }).start(() => {
+
+            this.setState({ currentIndex: this.state.currentIndex + 1 })
+            this.position.setValue({ x: 0, y: 0 })
+
+          })
+        }
+        else {
+          Animated.spring(this.position, {
+            toValue: ({ x: 0, y: 0 })
+          }).start()
+        }
+      }
+    })
+
+  }
+
+  renderArticles = () => {
+
+    return ARTICLES.map((item, i) => {
+
+      if (i < this.state.currentIndex) {
+        return null
+      }
+      if (i == this.state.currentIndex) {
+
+        return (
+
+          <Animated.View key={item.id} style={this.position.getLayout()}
+            {...this.PanResponder.panHandlers}
+          >
+            <View style={{ flex: 1, position: 'absolute', height: SCREEN_HEIGHT, width: SCREEN_WIDTH, backgroundColor: 'white' }}>
+
+              <View style={{ flex: 2, backgroundColor: 'black' }}>
+                <Image source={ARTICLES[i].uri}
+                  style={{ flex: 1, height: null, width: null, resizeMode: 'center' }}
+                ></Image>
+              </View>
+              <View style={{ flex: 3, padding: 5 }}>
+                <Text>
+                Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.     
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.  
+                </Text>
+              </View>
+            </View>
+          </Animated.View>
+        )
+      }
+      else {
+
+        return (
+          <Animated.View key={item.id}
+
+          >
+            <View style={{ flex: 1, position: 'absolute', height: SCREEN_HEIGHT, width: SCREEN_WIDTH, backgroundColor: 'white' }}>
+
+              <View style={{ flex: 2, backgroundColor: 'black' }}>
+                <Image source={ARTICLES[i].uri}
+                  style={{ flex: 1, height: null, width: null, resizeMode: 'center' }}
+                ></Image>
+              </View>
+              <View style={{ flex: 3, padding: 5 }}>
+                <Text>
+                Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.     
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.
+                  Lorem ipsum dolor sit amet, 
+                  mei et quidam tibique urbanitas, omnes 
+                  soleat signiferumque in cum. 
+                  His minim postea ei. Pri ei esse libris 
+                  placerat, mel an ullum maluisset. 
+                  Ad nam oportere similique. Cu his discere 
+                  erroribus accommodare, impedit appellantur 
+                  mediocritatem no has. Sit ut partiendo adversarium.  
+                </Text>
+              </View>
+            </View>
+          </Animated.View>
+        )
+
+      }
+    }).reverse()
+
+  }  
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1 }}>
+        {this.renderArticles()}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
